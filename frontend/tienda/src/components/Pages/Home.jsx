aad9eeb6657c4b2ba65891700775ab9466be8826
jhon170920 <j276scc@gmail.comclear>
2025-12-15 12:58:01 -0500

    subir

---
+++ frontend/tienda/src/components/Pages/Home.jsx
@@ -0,0 +1,7 @@
+import Navbar from "./components/Layout/Navbar.jsx";
+import Hero from "./components/Home/hero.jsx";
+import Categories from "./components/Home/categorias.jsx";
+import Footerpage from "./components/Layout/Footer.jsx";
+import Newsletter from "../Home/Newsletter.jsx";
+import FeaturedProducts from "../Home/Feactureproducts.jsx";
+
